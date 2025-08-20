e70353535884b8bc7c006fc84d249447aeb8f837
dquang0504 <deadlygamer686@gmail.com>
2025-08-20 16:26:15 +0700

    Update vite.config.ts

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -1,18 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(async () => {
+  const tailwindcss = (await import('@tailwindcss/vite')).default
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    host: true,                  // Cho phép truy cập từ bên ngoài
-    allowedHosts: ['.ngrok-free.app'] // Cho phép tất cả subdomain của ngrok-free.app
+    server: {
+      host: true, // Cho phép truy cập từ bên ngoài
+      allowedHosts: ['.ngrok-free.app'], // Cho phép tất cả subdomain của ngrok-free.app
+    },
   }
 })
