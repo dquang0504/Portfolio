e0348c1af7769544d00314b0381b490c89a2e8f2
dquang0504 <deadlygamer686@gmail.com>
2025-08-20 15:37:44 +0700

    changed api format from .ts to .cjs to avoid problems with the aforementioned conflict

---
+++ portfolio/api/contact.cjs
@@ -0,0 +1,35 @@
+const nodemailer = require('nodemailer');
+
+module.exports = async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  const { name, email, message } = req.body;
+
+  if (!name || !email || !message) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.GMAIL_USERNAME,
+        pass: process.env.GMAIL_PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: process.env.GMAIL_USERNAME,
+      to: process.env.GMAIL_USERNAME,
+      subject: `Portfolio Contact from ${name}`,
+      text: `From ${name} <${email}>\n\nMessage:\n${message}`,
+    });
+
+    return res.status(200).json({ success: true, message: 'Email sent!' });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Failed to send email!' });
+  }
+};
